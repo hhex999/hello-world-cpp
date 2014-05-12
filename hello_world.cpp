83c1ea3acce558b1baa0a1aa718bbd9192e060ce
hhex999 <hhex999@gmail.com>
2014-05-12 12:59:38 +0400

    5

--- hello_world.cpp
+++ hello_world.cpp
@@ -8,28 +8,30 @@ int main()
   int loops = 0;
   int backedUpLoops = 0;
 
-  std::cout << "\nType '1' to see at the looping 'hello world' string, "
+  std::cout << "\nType '1' to look at the looping 'hello world' string, "
 	    << "\notherwise type 'any symbol' to quit from the program: "
 	    << std::endl;
   std::cin >> choice;
 
   if (choice == 1) {
-    std::cout << "\nType the number of how many times do you want to watch "
-	      << "\nthe looping 'hello world' string: "
+    std::cout << "\nType the number of how many times do you want "
+	      << "\nto see the looping 'hello world' string: "
 	      << std::endl;
     std::cin >> loops;
 
     backedUpLoops = loops;
 
     for (; loops; loops--) {
-      std::cout << loops << ": Hello, world! This is SPARTAAA..."
+      std::cout << loops << " : Hello, world! This is SPARTAAA..."
 		<< std::endl;
     }
 
     std::cout << "\nHad been printed: " << backedUpLoops << " loops.";
     std::cout << "\nHave a nice day!" << std::endl;
+
   } else {
       std::cout << "\nYou have choosen to quit, bye bye then." << std::endl;
   }
+
     return 0;
 }
