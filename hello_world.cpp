1a8570dd295637c1814d5116aa1378a995bd9041
hhex999 <hhex999@gmail.com>
2014-07-27 00:08:16 +0400

    message

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,38 +1,26 @@
-// 'Hello world' CPP simple example
-
-#include <iostream>
+#include<iostream>
 
 int main()
 {
-  int choice = 0;
-  int loops = 0;
-  int backedUpLoops = 0;
-
-  std::cout << "\nType '1' to look at the looping 'hello world' string, "
-	    << "\notherwise type 'any symbol' to quit from the program: "
-	    << std::endl;
-  std::cin >> choice;
-
-  if (choice == 1) {
-    std::cout << "\nType the number of how many times do you want "
-	      << "\nto see the looping 'hello world' string: "
-	      << std::endl;
-    std::cin >> loops;
-    std::cout << "\n";
-
-    backedUpLoops = loops;
-
-    for (; loops; loops--) {
-      std::cout << loops << " : Hello, world! This is SPARTAAA..."
-		<< std::endl;
+  int choice=0;
+  int loops=0;
+  int backedupLoops=loops;
+  std::cout<<"\nType '1' to look at the looping 'hello world' string,"
+           << "\notherwise type 'any symbol' to quit from the program.: ";
+  std::cin>>choice;
+  if(choice==1) {
+    std::cout<<"\nType the number of how many times do you want to see "
+             << "\nthe looping 'hello world' string: ";
+    std::cin>>loops;
+    backedupLoops=loops;
+    for(; loops; loops--) {
+        std::cout<<loops<<": Hello, world!"<<std::endl;
     }
-
-    std::cout << "\nHad been printed: " << backedUpLoops << " loops.";
-    std::cout << "\nHave a nice day!" << std::endl;
-
+    std::cout<<"Had been printed: "<<backedupLoops<<" loops."<<std::endl;
+    std::cout<<"\nHave a nice day :]"<<std::endl;
   } else {
-      std::cout << "\nYou have choosen to quit, bye bye then." << std::endl;
+      std::cout<<"You have choosen to quit, bye bye then."<<std::endl;
   }
-
-    return 0;
+  return 0;
 }
+
---
+++ hello_world1.cpp
@@ -0,0 +1,40 @@
+// 'Hello world' CPP simple example
+
+#include <iostream>
+
+int main()
+{
+    int choice = 0;
+    int loops = 0;
+    int backedUpLoops = 0;
+
+    std::cout << "\nType '1' to look at the looping 'hello world' string, "
+              << "\notherwise type 'any symbol' to quit from the program: "
+              << std::endl;
+    std::cin >> choice;
+
+    if (choice == 1) {
+        std::cout << "\nType the number of how many times do you want "
+                  << "\nto see the looping 'hello world' string: "
+                  << std::endl;
+        std::cin >> loops;
+
+        backedUpLoops = loops;
+
+        for (; loops; loops--) {
+            std::cout << loops
+                      << " : Hello, world!"
+                      << std::endl;
+        }
+
+        std::cout << "\nHad been printed: " << backedUpLoops
+                  << " loops.";
+        std::cout << "\nHave a nice day!" << std::endl;
+    } else {
+        std::cout << "\nYou have choosen to quit, bye bye then."
+                  << std::endl;
+    }
+
+    return 0;
+}
+
